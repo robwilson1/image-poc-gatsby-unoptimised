35dc5a88f750a7791b5637ceeb06a3bb5985f09d
Rob Wilson <robwilson101@gmail.com>
2021-10-08 15:26:27 +0100

    Initial commit

---
+++ src/containers/home/Home.container.jsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import styles from './home.module.css'
+
+const HomeContainer = () => (
+  <main className={styles.main}>
+    <title>Gatsby | Unoptimised Images</title>
+    <h1 className={styles.heading}>
+      Gatsby Unoptimised Images
+    </h1>
+  </main>
+)
+
+export default HomeContainer;
